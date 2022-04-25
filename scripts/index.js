2490f992125088cb1c2b4fadbbf3b0f32ccdbd12
priyobratobanik <reach2priyo@gmail.com>
2022-04-25 15:27:44 +0530

    new text

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+// Store the wallet amount to your local storage with key "amount"
+function value(id){
+    return document.getElementById(id).value;
+  }
+  var amount = localStorage.getItem("amount")||0;
+      let wallet = document.getElementById("wallet");
+      wallet.innerText = amount;
+      
+  function addMoney(){
+      let inp_amount = value("amount");
+      amount = Number(amount) + Number(inp_amount)
+     
+      wallet.innerText = amount;
+      localStorage.setItem("amount",amount)
+  }
+  
+  
